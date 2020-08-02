68a9112e07f07c331109bd79b62ba8fb152e8c4a
michelle <foleymichelle9@gmail.com>
2020-08-01 20:33:08 -0500

    Set up project

---
+++ spec/requests/api/v1/request_response_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "Weather API", type: "request" do
+  describe "GET api/v1/weather" do
+    it "Can return weather info" do
+
+      get "/api/v1/weather?location=denver"
+
+      expect(response).to be_successful
+      weather = JSON.parse(response.body)
+      expect(weather["data"]["attributes"]).not_to be_empty
+      require "pry"; binding.pry
+    end
+  end
+end
