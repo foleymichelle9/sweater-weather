7a14b98f81458582c832ed132e463037d8142c6a
michelle <foleymichelle9@gmail.com>
2020-08-03 10:47:19 -0500

    test, routes, and start controllr

---
+++ app/controllers/api/v1/trail_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::TrailController < ApplicationController
+  def show
+
+  end
+end
---
+++ spec/requests/api/v1/forecast_hike_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Forecast trail Request" do
+  it "Destination Forecast and trail Request" do
+    get 'https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200861544-15f8ac02e786029340b0755817d0076f'
+
+    expect(response).to be_successful
+
+    parsed_hike_response = JSON.parse(response.body, symbolize_names: true)
+  end
+end
