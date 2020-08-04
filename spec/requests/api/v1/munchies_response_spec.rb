ca215da62467906b011b86e6c0cee168088b7a5b
michelle <foleymichelle9@gmail.com>
2020-08-04 12:34:34 -0500

    add more robust testing

--- spec/requests/api/v1/munchies_response_spec.rb
+++ spec/requests/api/v1/munchies_response_spec.rb
@@ -18,5 +18,17 @@ RSpec.describe "Munchie Request" do
     expect(parsed_munchies_response[:data][:attributes]).to have_key(:restaurant)
     expect(parsed_munchies_response[:data][:attributes][:restaurant]).to have_key(:name)
     expect(parsed_munchies_response[:data][:attributes][:restaurant]).to have_key(:address)
+
+    expect(parsed_munchies_response[:data]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:id]).to eq("null")
+    expect(parsed_munchies_response[:data][:type]).to eq("munchies")
+    expect(parsed_munchies_response[:data][:attributes]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:attributes][:start_location]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:attributes][:end_location]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:attributes][:destination_forecast]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:attributes][:destination_forecast][:current]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:attributes][:restaurant]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:attributes][:restaurant][:name]).to_not eq(nil)
+    expect(parsed_munchies_response[:data][:attributes][:restaurant][:address]).to_not eq(nil)
     end
   end
